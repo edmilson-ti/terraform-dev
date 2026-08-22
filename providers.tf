af3becf198468633f4f078a74ab6ce225f5cbf08
Edmilson Lima <edmilsonlima.cloud@gmail.com>
2026-08-22 12:04:24 -0300

    chore: adiciona pipeline de testes, providers e gitignore

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # =========================================================================
+  # CONFIGURAÇÃO DO BACKEND REMOTO (S3)
+  # ATENÇÃO: Deixe este bloco comentado com '#' até que você crie o bucket 
+  # fisicamente na AWS usando o comando 'terraform apply'.
+  # =========================================================================
+  # backend "s3" {
+  #   bucket         = "nome-do-seu-bucket-de-state-unico" # Nome exato do bucket S3
+  #   key            = "dev/terraform.tfstate"            # Caminho e nome do arquivo de estado dentro do bucket
+  #   region         = "us-east-1"                         # Mesma região do seu provider
+  #   encrypt        = true                                # Ativa criptografia nos arquivos de state
+  #   dynamodb_table = "terraform-state-lock"              # (Opcional) Tabela DynamoDB para evitar travas concorrentes
+  # }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
